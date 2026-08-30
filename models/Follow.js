24a761e1e36086001c8f1c08fc432fa6259a330d
Vishvagurusamratking <jaiprakashkushvaha130@gmail.com>
2026-08-30 21:23:33 +0530

    Add Follow model

---
+++ models/Follow.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+
+const followSchema = new mongoose.Schema({
+  followerId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  },
+  followingId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  },
+  status: {
+    type: String,
+    enum: ['pending', 'accepted'],
+    default: 'accepted'
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+module.exports = mongoose.model('Follow', followSchema);
