d17db297ceeb81e62acc3d8ddd5850da55823528
Vishvagurusamratking <jaiprakashkushvaha130@gmail.com>
2026-08-30 21:24:46 +0530

    Add Message model

---
+++ models/Message.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose');
+
+const messageSchema = new mongoose.Schema({
+  senderId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  },
+  recipientId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  },
+  text: {
+    type: String,
+    required: true
+  },
+  isRead: {
+    type: Boolean,
+    default: false
+  },
+  deletedBy: [mongoose.Schema.Types.ObjectId],
+  createdAt: {
+    type: Date,
+    default: Date.now,
+    index: true
+  }
+});
+
+module.exports = mongoose.model('Message', messageSchema);
